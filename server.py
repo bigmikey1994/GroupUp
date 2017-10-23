99b2b16119f6d1ab791c31758db90f121ec44e6b
JustNotAFox <JustNotAFox@gmail.com>
2017-10-23 00:19:38 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,38 @@
+import asyncio
+import websockets
+import json
+
+LISTEN_ADDRESS = ('0.0.0.0', 8080) #TODO: Abstract into a config file, but keep default
+clients = {}
+
+@asyncio.coroutine
+def client_handler(websocket, path):
+	print('New client', websocket)
+	print(' ({} existing clients)'.format(len(clients)))
+	name = yield from websocket.recv()
+	print(websocket, 'is now known as', name)
+	#yield from websocket.send('Welcome to websocket-chat {}'.format(name))
+	#yield from websocket.send('There are {} other users connected: {}'.format(len(clients), list(clients.values())))
+	yield from websocket.send(json.dumps({"command": "init", "users": list(clients.values())}))
+	clients[websocket] = name
+	for client, _ in clients.items():
+		#yield from client.send(name + 'has joined the chat')
+		yield from client.send(json.dumps({"command": "connect", "user": name}))
+	while True:
+		message = yield from websocket.recv()
+		if message == None:
+			their_name = clients[websocket]
+			del clients[websocket]
+			print('Client closed connection', websocket)
+			for client, _ in clients.items():
+				#yield from client.send(their_name + ' has left the chat')
+				yield from client.send(json.dumps({"command": "disconnect", "user": name}))
+			break
+		print(name + ':' + message)
+		for client, _ in clients.items():
+			#yield from client.send('{}:{}'.format(name, message))
+			yield from client.send(json.dumps({"command": "message", "user": name, "message": message}))
+	
+start_server = websockets.serve(client_handler, *LISTEN_ADDRESS)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
